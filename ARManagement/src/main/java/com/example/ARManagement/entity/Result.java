a55e74921c0d4f3ca9a1c8931dae7af8eab2094f
kavindu ishara <kavinduishara2923@gmail.com>
2025-03-02 00:35:58 +0530

    semester automation

--- ARManagement/src/main/java/com/example/ARManagement/entity/Result.java
+++ ARManagement/src/main/java/com/example/ARManagement/entity/Result.java
@@ -14,6 +14,7 @@ public class Result {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long resultId;
 
+
     @Column(name = "grade", nullable = false)
     private Grade grade;
 
